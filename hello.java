54901656fbbe59b4a3cc8a3d7f328db19a5f7113
manitdoshi <manitdoshi2004@gmail.com>
2023-01-23 02:34:17 +0530

    changed hello world program to ask name and greet user

--- hello.java
+++ hello.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class hello{
 public static void main(String[] args)
  {
-   System.out.println("Hello World"); 
+  System.out.println("Hello World");
+  Scanner sc= new Scanner (System.in);
+  System.out.println("What is your name?");
+  String str=sc.nextLine();
+
+   System.out.println("Hello "+str+ "\n"); 
 }
 }
